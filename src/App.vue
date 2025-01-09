cdf8ef98631f41a478f0130d9c9fe5b44e74a9b7
Anton <fynjn77@gmail.com>
2025-01-10 00:15:40 +0300

    исходя из вводных данных считаю тестовое задание выполненным

--- src/App.vue
+++ src/App.vue
@@ -7,38 +7,35 @@
         {{ item }}
       </th>
     </thead>
-    <tbody class="table__body" v-for="(value, index) in table">
-      <!-- <el-collapse v-model="activeNames" @change="handleChange">
-        <el-collapse-item title="Consistency" name='{{ index }}'>
-        </el-collapse-item>
-      </el-collapse> -->
-      <tr class="table__row" v-on:click="openGraphic(index)">
-        <td class="table__row-title" scope="row">
-          {{ value.label }}
-        </td>
-        <td class="table__row-today">
-          {{ prettify(days[0].today[index]) }}
-        </td>
-        <td class="table__row-yesterday">
-          {{ prettify(days[1].yesterday[index]) }}
-          <span v-bind:class='{
-            "red": relate < 0,
-            "green": relate > 0,
-          }'>
-            {{ Math.round(currentRelate(days[0].today[index], days[1].yesterday[index])) }} %</span>
+    <tbody>
+      <template class="table__body" v-for="(value, index) in table">
+        <tr class="table__row" v-on:click="openGraphic(index)">
+          <td class="table__row-title" scope="row">
+            {{ value.label }}
+          </td>
+          <td class="table__row-today">
+            {{ prettify(value.data[0]) }}
+          </td>
+          <td class="table__row-yesterday">
+            {{ prettify(value.data[1]) }}
+            <span v-bind:class='{
+              "red": currentRelate(value.data[0], value.data[1]) < 0,
+              "green": currentRelate(value.data[0], value.data[1]) > 0,
+            }'>
+              {{ currentRelate(value.data[0], value.data[1]) }} %</span>
+          </td>
+          <td class="table__row-day-of-week">
+            {{ prettify(dayOfWeek[index]) }}
+          </td>
 
-        </td>
-        <td class="table__row-day-of-week">
-          {{ prettify(days[0].today[index]) }}
-        </td>
-
-      </tr>
-      <tr>
-        <td colspan="4" v-if="selected == index">
-          <highcharts :options="chartOptions"></highcharts>
-        </td>
-      </tr>
-
+        </tr>
+        <tr>
+          <td colspan="4" v-if="selected === index">
+            <!-- <highcharts :options="chartOptions"></highcharts> -->
+            <highcharts :options="getOptions(value)"></highcharts>
+          </td>
+        </tr>
+      </template>
 
     </tbody>
   </table>
@@ -80,25 +77,51 @@ export default {
     return {
       test: 10,
       selected: false,
-      relate: false,
       columns: ['Показатель', 'Текущий день', 'Вчера', 'Этот день недели'],
       table: [
-        { label: 'Выручка, руб' },
-        { label: 'Надичные' },
-        { label: 'Безналичный рассчет' },
-        { label: 'Кредитные карты' },
-        { label: 'Средний чек, руб' },
-        { label: 'Средний гость, руб' },
-        { label: 'Удаление из чека(после оплаты, руб)' },
-        { label: 'Удаление из чека(до оплаты, руб)' },
-        { label: 'Количество чеков' },
-        { label: 'Количество гостей' },
+        {
+          label: 'Выручка, руб',
+          data: [500521, 480521, 836430, 292804, 702360, 921885, 914501,],
+        },
+        {
+          label: 'Наличные',
+          data: [300000, 300000, 252706, 154128, 796840, 200643, 402370],
+        },
+        {
+          label: 'Безналичный рассчет',
+          data: [100000, 100000, 563553, 659358, 610986, 380508, 245224],
+        },
+        {
+          label: 'Кредитные карты',
+          data: [100521, 100521, 601236, 106063, 979514, 327537, 532547],
+        },
+        {
+          label: 'Средний чек, руб',
+          data: [1300, 900, 5672, 8048, 4364, 4853, 6495],
+        },
+        {
+          label: 'Средний гость, руб',
+          data: [1200, 800, 5520, 2936, 1026, 8595, 8389],
+        },
+        {
+          label: 'Удаление из чека(после оплаты, руб)',
+          data: [1000, 1100, 5692, 4403, 2454, 6372, 5643],
+        },
+        {
+          label: 'Удаление из чека(до оплаты, руб)',
+          data: [1300, 1300, 6798, 3192, 7359, 5289, 8719],
+        },
+        {
+          label: 'Количество чеков',
+          data: [34, 34, 51, 67, 40, 49, 27],
+        },
+        {
+          label: 'Количество гостей',
+          data: [36, 36, 58, 19, 33, 57, 68],
+        },
       ],
-      days: [
-        { today: [500521, 300000, 100000, 100521, 1300, 1200, 1000, 1300, 34, 34] },
-        { yesterday: [480521, 300000, 100000, 100521, 900, 800, 1100, 1300, 36, 36] },
-        { dayBeforeYesterday: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] }
-      ],
+      // days: [
+
 
       dayOfWeek: [4805121, 300000, 100000, 100521, 900, 800, 900, 900, 34, 32],
       chartOptions: {
@@ -121,14 +144,12 @@ export default {
   methods: {
     currentRelate(today, yesterday) {
       this.relate = (today - yesterday) / yesterday * 100
-      return (today - yesterday) / yesterday * 100
+      return Math.round((today - yesterday) / yesterday * 100)
     },
-    newGraph(value, key) {
-      this.$set(
-        this.chartOptions.series[0].data = 100
-        // index,
-        // (Math.random() * 1000) | 0
-      );
+    getOptions(graphEl) {
+      const templateCopy = JSON.parse(JSON.stringify(this.chartOptions));
+      templateCopy.series[0].data = graphEl.data;
+      return templateCopy;
     },
     prettify(num) {
       let n = num.toString();
@@ -139,13 +160,6 @@ export default {
     }
   },
   watch: {
-    title(newValue) {
-      if (newValue === '') {
-        this.chartOptions.title.text = 'Entire title'
-      } else {
-        this.chartOptions.title.text = newValue
-      }
-    }
   }
 }
 
